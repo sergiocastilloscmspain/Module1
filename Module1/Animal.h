f3ba8d737f4f140ea027ebebbe8e57dc1fa1198d
sergiocastilloscmspain <sergio.castillo@scmspain.com>
2015-02-21 10:04:34 +0100

    upload animal

---
+++ Module1/Animal.h
@@ -0,0 +1,13 @@
+//
+//  Animal.h
+//  Module1
+//
+//  Created by sergio on 21/2/15.
+//  Copyright (c) 2015 sergio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Animal : NSObject
+
+@end
---
+++ Module1/Animal.m
@@ -0,0 +1,13 @@
+//
+//  Animal.m
+//  Module1
+//
+//  Created by sergio on 21/2/15.
+//  Copyright (c) 2015 sergio. All rights reserved.
+//
+
+#import "Animal.h"
+
+@implementation Animal
+
+@end
