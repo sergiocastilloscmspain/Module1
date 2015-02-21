7492e5cc1ce3ee58eb6307a373f19e45ba00d527
sergiocastilloscmspain <sergio.castillo@scmspain.com>
2015-02-21 09:41:04 +0100

    First commit

---
+++ Module1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Module1
+//
+//  Created by sergio on 21/2/15.
+//  Copyright (c) 2015 sergio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Module1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Module1
+//
+//  Created by sergio on 21/2/15.
+//  Copyright (c) 2015 sergio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
